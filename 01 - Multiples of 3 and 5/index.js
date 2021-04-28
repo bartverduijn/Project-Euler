c0b05f6938294ad3df2f461c82367692d90549fa
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2021-04-28 21:17:21 +0200

    Challenge 1 💪

---
+++ 01 - Multiples of 3 and 5/index.js
@@ -0,0 +1,21 @@
+/*
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+ */
+
+function multiplesOf3and5(number) {
+    let total = 0;
+    for (let i = 0; i < number; i += 1) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            total += i;
+        }
+    }
+    return total;
+}
+
+console.log(multiplesOf3and5(10));
+console.log(multiplesOf3and5(49));
+console.log(multiplesOf3and5(1000));
+console.log(multiplesOf3and5(8456));
+console.log(multiplesOf3and5(19564));
