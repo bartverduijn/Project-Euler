b96239ba587d00ad4c6060eeaff1d27c638478c2
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2021-04-28 21:46:12 +0200

    add linting and prettier 💅

--- 01 - Multiples of 3 and 5/index.js
+++ 01 - Multiples of 3 and 5/index.js
@@ -5,13 +5,13 @@
  */
 
 function multiplesOf3and5(number) {
-    let total = 0;
-    for (let i = 0; i < number; i += 1) {
-        if (i % 3 === 0 || i % 5 === 0) {
-            total += i;
-        }
-    }
-    return total;
+	let total = 0;
+	for (let i = 0; i < number; i += 1) {
+		if (i % 3 === 0 || i % 5 === 0) {
+			total += i;
+		}
+	}
+	return total;
 }
 
 console.log(multiplesOf3and5(10));
