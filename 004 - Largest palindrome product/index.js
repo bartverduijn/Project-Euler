4fcffbd0b1df4753be88d0b9695f05c4019d5e07
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2021-04-30 21:29:28 +0200

    added min value

--- 004 - Largest palindrome product/index.js
+++ 004 - Largest palindrome product/index.js
@@ -7,16 +7,17 @@
 
 function largestPalindromeProduct(n) {
 	let lpp = null;
-	const largestNum = parseInt('9'.repeat(n));
+	const max = parseInt('9'.repeat(n));
+	const min = (max + 1) / 10;
 
-	for (let i = largestNum; i > 0; i -= 1) {
-		for (let j = largestNum; j > 0; j -= 1) {
-			const p = i * j;
+	for (let i = max; i > min; i -= 1) {
+		for (let j = max; j > min; j -= 1) {
+			const prd = i * j;
 			// not interested if the product is smaller than the llpp
-			if (p < lpp) break;
+			if (prd < lpp) break;
 			// if the product is the same as the reverse, it's a palindrome
-			if (p === parseInt(p.toString().split('').reverse().join(''))) {
-				if (p > lpp) lpp = p;
+			if (prd === parseInt(prd.toString().split('').reverse().join(''))) {
+				if (prd > lpp) lpp = prd;
 				// no need to continue, as every product after this will me smaller.
 				break;
 			}
