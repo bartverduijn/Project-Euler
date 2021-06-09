2fb98b6a90e17b15a4b0226d413103296c9dff7b
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2021-06-09 20:08:53 +0200

    Very naive number counting, but it works

---
+++ 040 - Champernowne's constant/index.js
@@ -0,0 +1,33 @@
+/*
+	An irrational decimal fraction is created by concatenating the positive integers:
+	0.123456789101112131415161718192021...
+
+	It can be seen that the 12th digit of the fractional part is 1.
+
+	If dn represents the nth digit of the fractional part, find the value of the following expression.
+	d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+ */
+
+function champernownesConstant(n) {
+	let product = 1;
+	let counter = 0;
+
+	for (let i = 1; i <= n; i += 1) {
+		const number = String(i).split('');
+		for (let j = 0; j < number.length; j += 1) {
+			counter += 1;
+			// If the log10(counter) is a whole number, meaning it's 10, 100, 1000 etc.
+			if (
+				Math.floor(Math.log10(counter)) ===
+				Math.ceil(Math.log10(counter))
+			) {
+				product *= number[j];
+			}
+		}
+	}
+	return product;
+}
+
+console.log(champernownesConstant(100));
+console.log(champernownesConstant(1000));
+console.log(champernownesConstant(1000000));
